755a4a1361e3656b697c3cd61800e34fab64574b
TheEntropyShard <theentropyshard@gmail.com>
2025-05-05 21:05:25 +0300

    Added coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.coil.compose)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
